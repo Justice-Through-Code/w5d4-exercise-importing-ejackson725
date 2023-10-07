752692c170daeaf5f09078ab985fa8aa31bbb4d6
ejackson725 <ephraimj6@gmail.com>
2023-10-06 23:30:07 -0400

    wow

--- playlist.py
+++ playlist.py
@@ -6,15 +6,18 @@
 
 # 1.0 TODO: Import all of the functions in playlist_helpers.py into this file
 
+from playlist_helpers import * 
+import numpy as np
+
+
+
 
 # This code initializes your playlist as an empty list. No songs in it yet!
-my_playlist = []
-
-
+my_playlist_helpers = []
 # 2.0 TODO: Check what is in your playlist using the display_playlist() function
 # NOTE: Look at the display_playlist() function in playlist_helpers.py to figure out how to use it
 
-
+display_playlist(my_playlist_helpers)
 # 3.0 TODO: Add a song to my_playlist using the add_song() function
 # The song that you add should be a dictionary, with the following key-value pairs
 # 'artist' (string)
@@ -22,28 +25,35 @@ my_playlist = []
 # NOTE: Your songs can be whatever you want! The tests will check your FUNCTIONS with their own
 # input, not your print statements (:
 
+my_song = {"artist": "Ma$e", "title": "Welcome Back"}
+add_song(my_playlist_helpers, my_song)
+
+
 '''
 example_song = {'artist': 'Lauryn Hill', 'title': 'Everything Is Everything'}
 '''
 
 
 # 4.0 TODO: Check that you've added the song by running the display_playlist() function again
-
+display_playlist(my_playlist_helpers)
 
 
 # 5.1 TODO: Add 2 more songs to my_playlist (using the add_song function)
-
-
+my_song2 = {"artist": "Kanye", "title": "Jesus Walks"}
+my_song3 = {"artist": "Pop Smoke", "title": "Welcome to the Party"}
+add_song(my_playlist_helpers, my_song2)
+add_song(my_playlist_helpers, my_song3)
 # 5.2 TODO: Then display it again using the display_playlist() function
 
-
+display_playlist(my_playlist_helpers)
 # 6.1 TODO: In playlist_helpers.py, define a function called get_playlist_length()
 # See playlist_helpers.py for details on how to define this function
 
 
 # 6.2 TODO: Call the get_playlist_length function you just created in THIS script
 # to get the length of my_playlist (make sure you print out the result here!)
-
+playlist_length = get_playlist_length(my_playlist_helpers)
+print(playlist_length)
 
 # 7.0 TODO: At the top of this script, import numpy using the usual alias
 
@@ -52,8 +62,12 @@ example_song = {'artist': 'Lauryn Hill', 'title': 'Everything Is Everything'}
 # 8.0 TODO: using the mean() function from numpy, calculate and print the average of monthly_plays
 # You don't have to write any functions for this question
 monthly_plays = [127030, 274920, 232453, 98278, 500301, 235462]
-
-
+average_plays = np.mean(monthly_plays)
+print(average_plays)
 # 9.0 TODO: In playlist_helpers.py, define a new function called play_track()
 # See playlist_helpers.py for details on how to define this function
 # In this file, play a few tracks, and run display_playlist() again to make sure it works
+
+play_track(my_playlist_helpers, 1)
+play_track(my_playlist_helpers, 2)
+play_track(my_playlist_helpers, 3)
